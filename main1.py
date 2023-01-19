2c263703ce0961c1fc8718024650ce0a7a8bcc88
Kirill Efremov <stchristoff82@gmail.com>
2023-01-19 17:54:36 +0300

    Add new file main1.py

---
+++ main1.py
@@ -0,0 +1,29 @@
+class Character:
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def attack(self):
+        pass
+
+    def defence(self):
+        pass
+
+    def special(self):
+        pass
+
+
+
+
+class Warrior(Character):
+    pass
+
+
+
+class Mage(Character):
+    pass
+
+
+
+class Healer(Character):
+    pass
+
